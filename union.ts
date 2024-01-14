235a2504b96ff2322a1a87131734f2758f174fa4
Prabhat Sharma <ps75969@corp.cyient.com>
2024-01-14 14:31:28 +0530

    union

--- union.ts
+++ union.ts
@@ -1,5 +1,5 @@
 let a: number | string;
 a = 10;
-a = 'prabhat';
-
+a = 'Jhonson';
+console.log(a); //Jhonson
 //here variable can hold values of type number and strings, if we assign any other value to variable a like boolean it will give error.
